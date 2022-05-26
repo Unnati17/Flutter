dad062cc4c9116e104c952503efc5fabd91983ca
Unnati17 <67010484+Unnati17@users.noreply.github.com>
2022-05-26 18:31:46 +0530

    Add files via upload

---
+++ shop_app/flutter_application/lib/screens/main.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter_application_1/screens/home.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/screens/second.dart';
+// import 'package:flutter_application_1/screens/stack.dart';
+// //import 'package:flutter_application_1/screens/ted.dart';
+// import 'package:flutter_application_1/screens/third.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//       debugShowCheckedModeBanner: false
+//       // home: SafeArea(child: Text('Hello Flutter')),
+//       //home: Third()));
+//      // home: Ted();
+// }
